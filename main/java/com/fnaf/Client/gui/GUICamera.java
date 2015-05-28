e63b84d487a63ecdd14c3ca2d36718f4a30c1181
T0psid3xDev <altisimomods@gmail.com>
2015-05-27 21:19:24 -0700

    Fixed some bugs form last commit

--- main/java/com/fnaf/Client/gui/GUICamera.java
+++ main/java/com/fnaf/Client/gui/GUICamera.java
@@ -16,7 +16,7 @@ public class GUICamera extends GuiScreen {
 	private int page = 1;
 	
 	public GUICamera() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	
@@ -39,22 +39,17 @@ public class GUICamera extends GuiScreen {
 		for(int i = ((page * 5) - 3); i <= ((page * 5) + 1); i++){
 			if(i - 2 < cameraMonitor.getCameraPositions(nbtTag).size() && cameraMonitor.getCameraPositions(nbtTag).get(i - 2) != null){
 				this.buttonList.add(new GuiButton(i, -25 + ((counter) * 70), this.height - 45, 60, 20, "Camera #" + (i - 1)));
-			}
-			
+			}			
 			counter += 1;
 		}
 		
 		this.buttonList.add(new GuiButton(((page * 5) + 2), this.width - 25, this.height - 45, 20, 20, ">"));
 		
-		//TODO
-//		if(cameraMonitor.getCameraPositions(nbtTag).size() <= ((page * 5) + 1)){
-//			((GuiButton) this.buttonList.get(((page * 5) - 4))).visible = false;
-//			((GuiButton) this.buttonList.get(((page * 5) + 2))).visible = false;
-//		}
+	}
     }
 	
     public void onGuiClosed(){
-    	//ClientUtils.setCameraZoom(0D);
+    	
     }
 	
 	public void drawScreen(int par1, int par2, float par3){
@@ -62,27 +57,17 @@ public class GUICamera extends GuiScreen {
     }
 	
 	protected void actionPerformed(GuiButton guibutton){		
+		
 		if(guibutton.id == ((page * 5) - 4) && page > 1){
 			this.mc.displayGuiScreen(new GUICamera());
+		
 		}else if(guibutton.id == ((page * 5) + 2) && cameraMonitor.getCameraPositions(nbtTag).size() > ((page * 5) - 1)){
 			this.mc.displayGuiScreen(new GUICamera());
+	
 		}else if(guibutton.id > page && guibutton.id <= (page * 6) ){
-			//if(Minecraft.getMinecraft().thePlayer.ridingEntity == null){
-			//	System.out.println("Reseting pos");
-			//	mod_SecurityCraft.instance.setUsePosition(Minecraft.getMinecraft().thePlayer.getCommandSenderName(), Minecraft.getMinecraft().thePlayer.posX, Minecraft.getMinecraft().thePlayer.posY, Minecraft.getMinecraft().thePlayer.posZ, Minecraft.getMinecraft().thePlayer.rotationYaw, Minecraft.getMinecraft().thePlayer.rotationPitch);
-			//}
-		//	mod_SecurityCraft.network.sendToServer(new PacketMountCamera(cameraMonitor.getCameraPositions(nbtTag).get(guibutton.id - 2)[0], cameraMonitor.getCameraPositions(nbtTag).get(guibutton.id - 2)[1], cameraMonitor.getCameraPositions(nbtTag).get(guibutton.id - 2)[2], guibutton.id + 1));
-		//	Minecraft.getMinecraft().thePlayer.closeScreen();
+		
 		}
 		
-//		if(guibutton.id == ((page * 5) - 4) && page > 1){
-//			this.mc.displayGuiScreen(new GuiCameraMonitorOverlay(cameraMonitor, nbtTag, page -= 1));
-//		}else if(guibutton.id == ((page * 5) + 2) && cameraMonitor.getCameraPositions(nbtTag).size() > ((page * 5) - 1)){
-//			this.mc.displayGuiScreen(new GuiCameraMonitorOverlay(cameraMonitor, nbtTag, page += 1));
-//		}else if(guibutton.id > page && guibutton.id <= (page * 6) ){
-//			mod_SecurityCraft.network.sendToServer(new PacketMountCamera(cameraMonitor.getCameraPositions(nbtTag).get(guibutton.id - 1)[0], cameraMonitor.getCameraPositions(nbtTag).get(guibutton.id - 1)[1], cameraMonitor.getCameraPositions(nbtTag).get(guibutton.id - 1)[2]));
-//			Minecraft.getMinecraft().thePlayer.closeScreen();
-//		}
 	}
 
 	public boolean doesGuiPauseGame()
